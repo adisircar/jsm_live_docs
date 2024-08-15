1f2ee9fc7139236cafdb466d69997c5beb99c2e9
Adi <asircar9@gmail.com>
2024-08-15 17:18:56 +0400

    Update Editor.tsx

--- Editor.tsx
+++ Editor.tsx
@@ -11,6 +11,8 @@ import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
 import React from 'react';
 
+import { liveblocksConfig } from '@liveblocks/react-lexical';
+
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
 // try to recover gracefully without losing user data.
@@ -19,8 +21,8 @@ function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;
 }
 
-export function Editor() {
-  const initialConfig = {
+export function Editor({ roomId, currentUserType }: { roomId: string, currentUserType: UserType}) {
+  const initialConfig = liveblocksConfig({
     namespace: 'Editor',
     nodes: [HeadingNode],
     onError: (error: Error) => {
@@ -28,12 +30,15 @@ export function Editor() {
       throw error;
     },
     theme: Theme,
-  };
+    editable: currentUserType === 'editor',
+  });
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
       <div className="editor-container size-full">
-        <ToolbarPlugin />
+        <div className="toolbar-wrapper flex min-w-full justify-between">
+          <ToolbarPlugin />
+        </div>
 
         <div className="editor-inner h-[1100px]">
           <RichTextPlugin
